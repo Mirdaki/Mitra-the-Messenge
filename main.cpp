3bf4fff9f788318870cc21eae6722d1a8db7ffe9
konradpabjan <konradpabjan@outlook.com>
2016-04-13 17:41:04 -0400

    Update main.cpp
    Added more comments to make it clear what is going on. Simple formatting fixes.

--- main.cpp
+++ main.cpp
@@ -9,19 +9,17 @@ using namespace std;
 // input parameters are 2 strings, output is minimum number of swaps to get from 1 string to another 
 int min_swaps_needed(string first, string second){
 
-// 2D array is created to store the previously computed optimal solutions
+// Empty 2D array is created to store the previously computed optimal solutions
 	int swaps[first.length()+1][second.length()+1];
 
 // first row is set to 0,1,2,3.....first.length()-1	
 	for(int i=0; i<first.length()+1; i++){
-		swaps[i][0] = i;
-		}
-
+		swaps[i][0] = i;}
+		
 // first column is set to 0,1,2,3.... second.length()-1	
 	for(int j=0; j<second.length()+1; j++){
-		swaps[0][j] = j;
-		}
-/** after the first 2 operations we have something like this
+		swaps[0][j] = j;}
+/** after the above 2 operations we have something like this, the goal of the next 2 for loops is to build up the rest of the 2D array
  * 
  *  0 1 2 3 4 5
  *  1 0 0 0 0 0
@@ -30,28 +28,30 @@ int min_swaps_needed(string first, string second){
  * 
  **/
  
-// now we itterate through the remaining unassigned indexes in the 2D array
+// now we itterate through the rest of the 2D array and we build it bottom up using the initial row and column 
 		for(int i=1; i<second.length()+1; i++){
-		
 			for(int j=1; j<first.length()+1; j++){
 			
 // we compare 2 elements in the string
 				if(second.at(i-1) == first.at(j-1)){
-// we have a match, we use the previous optimal amount 
-					swaps[j][i] = swaps[j-1][i-1];
-					}
+// we have a match, we use the previous optimal amount which is in the top left adjacent location 
+					swaps[j][i] = swaps[j-1][i-1];}
+					
 // no match, we take the minimum of the three previous optimal solutions and add 1		
 				else{
-				int compare = min(swaps[j][i-1], swaps[j-1][i]);
-				int minimum = min(compare, swaps[j-1][i-1]);
-				swaps[j][i] = minimum+1;
-				}
-				
-				}
+// the min() function in C++ compares only 2 things at once so I had to run it twice since we have to find the min of 3 elements
+					int compare = min(swaps[j][i-1], swaps[j-1][i]);
+					int minimum = min(compare, swaps[j-1][i-1]);
+					swaps[j][i] = minimum+1;}
 			}
-// the number of swaps required is the number in the lower right of the 2D array 			
-			return swaps[first.length()][second.length()];
 		}
+// after the whole 2D array is built, the number of swaps required is the number in the lower right location of the 2D array 			
+	return swaps[first.length()][second.length()];
+			
+// space complextiy of this algorithm is O(N*M) 
+// time complextiy of this algorithm is O(N*M) 
+// where N is the length of string 1 and M is the length of String 2
+}
 
 int numGems(int steps, vector<int> mag){
 
