0b57a934223b32d1564f4fd1885ce0db9f145aca
cal2u <cal2u@ufl.edu>
2016-04-23 19:32:17 -0400

    Made formatting consistnet across sections

--- main.cpp
+++ main.cpp
@@ -25,56 +25,61 @@ struct compare {
 };
 
 // input parameters are 2 strings, output is minimum number of swaps to get from 1 string to another
-int min_swaps_needed(string first, string second){
+int min_swaps_needed(string first, string second) {
 
-// Empty 2D array is created to store the previously computed optimal solutions
+	// Empty 2D array is created to store the previously computed optimal solutions
 	int swaps[first.length()+1][second.length()+1];
 
-// first row is set to 0,1,2,3.....first.length()-1
-	for(int i=0; i<first.length()+1; i++){
-		swaps[i][0] = i;}
+	// first row is set to 0,1,2,3.....first.length()-1
+	for(int i = 0; i < first.length()+1; i++) {
+		swaps[i][0] = i;
+	}
 
-// first column is set to 0,1,2,3.... second.length()-1
-	for(int j=0; j<second.length()+1; j++){
-		swaps[0][j] = j;}
-/** after the above 2 operations we have something like this, the goal of the next 2 for loops is to build up the rest of the 2D array
- *
- *  0 1 2 3 4 5
- *  1 0 0 0 0 0
- *  2 0 0 0 0 0
- *  3 0 0 0 0 0
- *
- **/
+	// first column is set to 0,1,2,3.... second.length()-1
+	for(int j = 0; j < second.length()+1; j++) {
+		swaps[0][j] = j;
+	}
+	/** after the above 2 operations we have something like this, the goal of the next 2 for loops is to build up the rest of the 2D array
+	 *
+	 *  0 1 2 3 4 5
+	 *  1 0 0 0 0 0
+	 *  2 0 0 0 0 0
+	 *  3 0 0 0 0 0
+	 *
+	 **/
 
-// now we itterate through the rest of the 2D array and we build it bottom up using the initial row and column
-		for(int i=1; i<second.length()+1; i++){
-			for(int j=1; j<first.length()+1; j++){
+	// now we itterate through the rest of the 2D array and we build it bottom up using the initial row and column
+	for(int i = 1; i < second.length()+1; i++) {
+		for(int j = 1; j < first.length()+1; j++) {
 
-// we compare 2 elements in the string
-				if(second.at(i-1) == first.at(j-1)){
-// we have a match, we use the previous optimal amount which is in the top left adjacent location
-					swaps[j][i] = swaps[j-1][i-1];}
-
-// no match, we take the minimum of the three previous optimal solutions and add 1
-				else{
-// the min() function in C++ compares only 2 things at once so I had to run it twice since we have to find the min of 3 elements
-					int compare = min(swaps[j][i-1], swaps[j-1][i]);
-					int minimum = min(compare, swaps[j-1][i-1]);
-					swaps[j][i] = minimum+1;}
+			// we compare 2 elements in the string
+			if(second.at(i-1) == first.at(j-1)) {
+				// we have a match, we use the previous optimal amount which is in the top left adjacent location
+				swaps[j][i] = swaps[j-1][i-1];
+			}
+			
+			// no match, we take the minimum of the three previous optimal solutions and add 1
+			else {
+				// the min() function in C++ compares only 2 things at once so I had to run it twice since we have to find the min of 3 elements
+				int compare = min(swaps[j][i-1], swaps[j-1][i]);
+				int minimum = min(compare, swaps[j-1][i-1]);
+				swaps[j][i] = minimum+1;
 			}
 		}
-// after the whole 2D array is built, the number of swaps required is the number in the lower right location of the 2D array
+	}
+
+	// after the whole 2D array is built, the number of swaps required is the number in the lower right location of the 2D array
 	return swaps[first.length()][second.length()];
 
-// space complextiy of this algorithm is O(N*M)
-// time complextiy of this algorithm is O(N*M)
-// where N is the length of string 1 and M is the length of String 2
+	// space complextiy of this algorithm is O(N*M)
+	// time complextiy of this algorithm is O(N*M)
+	// where N is the length of string 1 and M is the length of String 2
 }
 
-vector<int> numGems(vector<int> mag){
+vector<int> numGems(vector<int> mag) {
 
 	int max = 0;
-	for(int i = 0; i<mag.size(); i++){
+	for(int i = 0; i<mag.size(); i++) {
 		if(mag[i] > max)
 			max = mag[i];
 	}
@@ -86,49 +91,44 @@ vector<int> numGems(vector<int> mag){
 
 
 	int r = mag.size()-1;										//start by filling in top row
-	for(int c = max; c>=0; c--)									//if this maggi's value is large enough, write a 1
+	for(int c = max; c >= 0; c--) {								//if this maggi's value is large enough, write a 1
 		memo[r][c] = (mag[r] >= c);
+	}	
 
 	r--;
 	int valAbove;
 	int takingThis;
-	while (r>=0){
-		for(int c = max; c>=0; c--){
+	while (r >=0 ) {
+		for (int c = max; c >= 0; c--) {
 
 			valAbove = memo[r+1][c];
 
-			if(mag[r] < c)											//if this maggi couldn't be selected regardless,
+			if (mag[r] < c) { 										//if this maggi couldn't be selected regardless,
 				takingThis = 0;										//don't select it
-			else{
+			}									
+			else {
 				takingThis = 1;
 
-				if(mag[r]+1 <= max){								//if this maggi can be taken while leaving other selections possible
+				if(mag[r]+1 <= max) {								//if this maggi can be taken while leaving other selections possible
 					takingThis += memo[r+1][mag[r]+1];				//add the max number of other selections from above
 				}
 			}
 
-			memo[r][c] = (takingThis>valAbove)?takingThis:valAbove;	//take the max of if you selected or rejected this maggi
+			memo[r][c] = (takingThis > valAbove) ? takingThis : valAbove;	//take the max of if you selected or rejected this maggi
 		}
 		r--;
 
 	}
 
-	//out(memo);
-
-	//if it took too many steps, return -1
-	// if(memo[0][0] < steps)
-	// 	// return -1;
-
-	//trace back
 	r = 0;
 	int c = 1;
 	int numberGems = 0;
 	vector<int> gemsUsed;
 	gemsUsed.push_back(0);
-	while(r<mag.size()-1){
+	while (r < mag.size()-1) {
 
 		//if you took this one, add his value to the gems
-		if(memo[r][c] > memo[r+1][c]){
+		if(memo[r][c] > memo[r+1][c]) {
 
 			numberGems += mag[r];
 
@@ -141,36 +141,29 @@ vector<int> numGems(vector<int> mag){
 		r++;
 	}
 
-
-
 	//if you took the last one, add it's gems
-	if(memo[r][c] == 1){
+	if (memo[r][c] == 1) {
 		numberGems += mag[r];
 		gemsUsed.push_back(numberGems);
 	}
-
-	// for(int i = 0; i<gemsUsed.size(); i++)
-	// 	cout << gemsUsed[i] << " ";
 
 	return gemsUsed;
 
 }
 
 // This will find the the shortest path if possible, and print out the results
-void dijkstra(Realm& start, Realm& final, vector<vector<int> >& graph, vector<Realm>& realms){
+void dijkstra(Realm& start, Realm& final, vector<vector<int> >& graph, vector<Realm>& realms) {
 
     // Minimum Priority Queue to find the next shortest path
     priority_queue<Realm*, vector<Realm*>, compare> minQue;
 
     // Assign start node a distance of zero and add it to queue
-    
     start.distance = 0;
-    //cout << "Start: " << start.numId << " Distance: " << start.distance << endl;
-
     minQue.push(&start);
 
     // Add all but start realm to unvisted list
     queue <Realm*> unvisted;
+
     for (int i = 0; i < realms.size(); i++){
         if (realms[i].name != start.name) unvisted.push(&realms[i]);
     }
@@ -180,7 +173,7 @@ void dijkstra(Realm& start, Realm& final, vector<vector<int> >& graph, vector<Re
     Realm *current;
 
     // The actual dijkstra algorithim
-    while (!minQue.empty()){
+    while (!minQue.empty()) {
 
         // Make current the next shortest path
         current = minQue.top();
@@ -196,69 +189,76 @@ void dijkstra(Realm& start, Realm& final, vector<vector<int> >& graph, vector<Re
         } */
 
         // Go through each unvisted Realm
-        for (int i = 0; i < unvisted.size();){ // May have an issue with size, seeing it's being chnaged in loop
-        	// If we're at the current node, ignore it, don't push it back on to the queue
-            if (unvisted.front()->numId != current->numId){ // May want to make this -1 instead of NULL
-                // Get the new distance
-                int newDistance = current->distance + graph[current->numId][unvisted.front()->numId];
-                // If new distance is less than previous, change previous and add to priority queue
-                if (newDistance < unvisted.front()->distance && unvisted.front()->gems_required.size() > graph[current->numId][unvisted.front()->numId]){
-                    unvisted.front()->lastRealmId = current->numId;
-                    unvisted.front()->distance = newDistance;
+        for (int i = 0; i < unvisted.size();) {
+        	// Popping them off the unvisited queue
+            Realm *to_visit = unvisted.front();
+            unvisted.pop();
 
+        	// If we're at the current node, ignore it; don't push it back on to the queue
+        	// because we can guarentee we already have the shortest distance to it
+
+            if (to_visit->numId != current->numId) { 
+                
+                // Calculate the new potential distance
+                int newDistance = current->distance + graph[current->numId][to_visit->numId];
+
+                // If new distance is less than previous best distance, change previous and add node to priority queue
+                if (newDistance < to_visit->distance && 
+                	to_visit->gems_required.size() > graph[current->numId][to_visit->numId]) {
+                    
+                    to_visit->lastRealmId = current->numId;
+                    to_visit->distance = newDistance;
+ 
                     // Add to priority queue
-                    minQue.push(unvisted.front());
+                    minQue.push(to_visit);
 
                 }
 
-                // Add realm back to unvisted queue
+                // Add realm back to unvisted queue, because it still might have a shorter path
                 unvisted.push(unvisted.front());
                 i++;
             }
-            // Pop unvisted queue
-            unvisted.pop();
         }
     }
-    //cout << current.distance << endl;
-
-    // // Print the results
-    // if (current.name != final.name){
-    //     cout << "IMPOSSIBLE";
-    // } else {
-    //     cout << current.distance;
-    // }
 }
 
-int main(){
+int main() {
+	// The number of realms in the world
 	int N;
 	cin >> N;
+
+	// Vector storing list of all realms
 	vector<Realm> realms;
 
+	// Input all realm info and store it
 	for (int i = 0; i < N; i++) {
 		Realm r;
 		string name;
 		cin >> name;
 
-		// Get the number of magi, and add them to a vector
+		// Get the number of magi per realm, and add them to a vector
 		vector<int> magi;
 		int num_magi;
 		cin >> num_magi;
-		//cout << "Num Magi: " << num_magi << endl;
+
 		for (int j = 0; j < num_magi; j++) {
 			int num;
 			cin >> num;
 			magi.push_back(num);
 		}
-		//cout << "Got magi" << endl;
+
+		// Assemble the realm object and add to vector
 		r.name = name;
 		r.magi = magi;
 		r.numId = i;
 		realms.push_back(r);
 	}
 
+	// Get start and destination realms
 	string start_name, end_name;
 	cin >> start_name >> end_name;
 
+	// Figure out what their indexes are
 	int start, end;
 	for (int i = 0; i < N; i++) {
 		if (start_name == realms[i].name) start = i;
@@ -267,6 +267,8 @@ int main(){
 
 	vector<vector<int> > graph( N, vector<int>(N,0) );
 
+
+	// Precompute graph edges
 	for (int i = 0; i < N; i++) {
 		// Calculate the number of gems required for a given number of incantations/swaps
 		realms[i].gems_required = numGems(realms[i].magi);
@@ -283,63 +285,48 @@ int main(){
 	// Do Dijkstras from end to start
 	dijkstra(realms[end], realms[start], graph, realms);
 
+	// See if we have a path
 	if (realms[start].distance == numeric_limits<int>::max()) {
 		cout << "IMPOSSIBLE" << endl;
 	} else {
 		int total_gems = 0;
 		Realm current = realms[start];
 
-		// Backtrace to find number of gems
+		// If we do, backtrace to find number of gems
 		while (current.numId != realms[end].numId) {
-
 			total_gems += current.gems_required[graph[current.lastRealmId][current.numId]];
-
 			current = realms[current.lastRealmId];
-			//cout << "Backtrack id " << current.numId << endl;
-
 		}
 
 		cout << realms[start].distance << " " << total_gems << endl;
 	}
 
-	// cout << "---- Grid -----" << endl;
-	// for (int i = 0; i < N; i++) {
-	// 	for (int j = 0; j < N; j++) {
-	// 		cout << graph[i][j] << " ";
-	// 	}
-	// 	cout << endl;
-	// }
 
-
-
-	// Reset distances
+	// Reset tentative distances
 	for (int i = 0; i < N; i++) {
-		//cout << "ID: " << i << " " << realms[i].distance << endl;
 		realms[i].distance = numeric_limits<int>::max();
 	}
 
 
-// Do Dijkstras from start to end
+	// Do Dijkstras from start to end
 	dijkstra(realms[start], realms[end], graph, realms);
 
+	// See if we have a path
 	if (realms[end].distance == numeric_limits<int>::max()) {
 		cout << "IMPOSSIBLE" << endl;
-	} else {
+	} 
+	else {
 		int total_gems = 0;
 		Realm current = realms[end];
 
-		// Backtrace to find number of gems
+		// If we do, backtrace to find number of gems
 		while (current.numId != realms[start].numId) {
 			total_gems += current.gems_required[graph[current.lastRealmId][current.numId]];
 			current = realms[current.lastRealmId];
-
-			//cout << "Backtrack id " << current.numId << endl;
 		}
 
 		cout << realms[end].distance << " " << total_gems << endl;
 	}
-
-
 
     return 0;
 }
